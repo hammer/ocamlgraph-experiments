49102eb0b968ca4b9bbf9e8b3375242a2396503a
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2015-01-13 15:06:35 -0500

    Don't expose the graph methods.

--- src/lib/suffix_tree.ml
+++ src/lib/suffix_tree.ml
@@ -1,14 +1,30 @@
 module ST = struct
-  (* TODO(hamer): Use sosa *)
-  (* TODO(hamer): Make terminator character explicit *)
-  module Str = struct
-    type t = string
-    let default = ""
-    let compare = Pervasives.compare
+  module G = struct
+    (* TODO(hamer): Use sosa *)
+    (* TODO(hamer): Make terminator character explicit *)
+    module Str = struct
+      type t = string
+      let default = ""
+      let compare = Pervasives.compare
+    end
+
+    (* TODO(hammer): Use int option for vertex labels *)
+    include Graph.Imperative.Digraph.AbstractLabeled(struct type t = unit end)(Str)
   end
 
-  (* TODO(hammer): Use int option for vertex labels *)
-  include Graph.Imperative.Digraph.AbstractLabeled(struct type t = int end)(Str)
+  type t = { g : G.t; leaves : G.V.t array }
+
+  let create s =
+    let g = G.create () in
+    let v1 = G.V.create () in
+    let v2 = G.V.create () in
+    let e = G.E.create v1 s v2 in
+    G.add_edge_e g e;
+    let leaves = [| v2 |]  in
+    { g; leaves }
+
+  let get_suffix st i =
+    G.fold_pred_e (fun e s -> (G.E.label e) ^ s) st.g st.leaves.(i) ""
 end
 
 
--- src/test/test_suffix_tree.ml
+++ src/test/test_suffix_tree.ml
@@ -2,13 +2,11 @@ open OUnit2
 open Suffix_tree
 
 let test_simple_st test_ctxt =
-  let g = ST.create () in
-  let v1 = ST.V.create (-1) in
-  let v2 = ST.V.create 0 in
-  let e1 = ST.E.create v1 "hey hey hey" v2 in
-  ST.add_edge_e g e1;
-  let s = ST.fold_pred_e (fun e s -> (ST.E.label e) ^ s) g v2 "" in
-  assert_equal "hey hey hey" s
+  let s = "hey hey hey" in
+  let st = ST.create s in
+  let derived_s = ST.get_suffix st 0 in
+  assert_equal s derived_s
+
 let suite =
   "suite" >:::
     [ "test_simple_st" >:: test_simple_st
