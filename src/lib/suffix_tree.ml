94fac57f923aedc3c203c9d1a771a79d48d803cd
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2015-01-15 22:17:36 -0500

    Slightly richer representation of a suffix tree. Time to actually build one.

--- src/lib/suffix_tree.ml
+++ src/lib/suffix_tree.ml
@@ -12,16 +12,22 @@ module ST = struct
     include Graph.Imperative.Digraph.AbstractLabeled(struct type t = unit end)(Str)
   end
 
-  type t = { g : G.t; leaves : G.V.t array }
+  type t = { g : G.t; root : G.V.t; leaves : G.V.t array }
 
   let create s =
+    let s_length = String.length s in
     let g = G.create () in
-    let v1 = G.V.create () in
-    let v2 = G.V.create () in
-    let e = G.E.create v1 s v2 in
-    G.add_edge_e g e;
-    let leaves = [| v2 |]  in
-    { g; leaves }
+    let root = G.V.create () in
+    let leaves = Array.make s_length (G.V.create ()) in
+    let st = { g; root; leaves } in
+    for i = 0 to s_length - 1 do
+      let leaf = G.V.create () in
+      let label = String.sub s i (s_length - i) in
+      let e = G.E.create st.root label leaf in
+      G.add_edge_e st.g e;
+      st.leaves.(i) <- leaf;
+    done;
+    st
 
   let get_suffix st i =
     G.fold_pred_e (fun e s -> (G.E.label e) ^ s) st.g st.leaves.(i) ""
--- src/test/test_suffix_tree.ml
+++ src/test/test_suffix_tree.ml
@@ -1,15 +1,19 @@
 open OUnit2
 open Suffix_tree
 
-let test_simple_st test_ctxt =
+let test_suffixes test_ctxt =
   let s = "hey hey hey" in
+  let s_length = String.length s in
   let st = ST.create s in
-  let derived_s = ST.get_suffix st 0 in
-  assert_equal s derived_s
+  for i = 0 to s_length - 1 do
+    let st_suffix = ST.get_suffix st i in
+    let suffix = String.sub s i (s_length - i) in
+    assert_equal suffix st_suffix
+  done
 
 let suite =
   "suite" >:::
-    [ "test_simple_st" >:: test_simple_st
+    [ "test_suffixes" >:: test_suffixes
     ]
 
 let () =
