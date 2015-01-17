12210b4e4c627e3c4ae37011f8c9e05b7003f6aa
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2015-01-17 17:29:51 -0500

    Make test actually traverse two edges.

--- src/test/test_suffix_tree.ml
+++ src/test/test_suffix_tree.ml
@@ -22,14 +22,17 @@ let test_lcp_path test_ctxt =
   ST.G.add_vertex g root;
   let leaves = Array.make 1 (ST.G.V.create ()) in
   let st:ST.t = { g; root; leaves } in
+  let inner_node = ST.G.V.create () in
+  let e1 = ST.G.E.create st.root "he" inner_node in
+  ST.G.add_edge_e st.g e1;
   let leaf = ST.G.V.create () in
-  let e = ST.G.E.create st.root "hey" leaf in
-  ST.G.add_edge_e st.g e;
+  let e2 = ST.G.E.create inner_node "y there" leaf in
+  ST.G.add_edge_e st.g e2;
   st.leaves.(0) <- leaf;
   let es = ST.G.succ_e st.g st.root in
-  let s_len, e_info = ST.lcp_path st "he" es in
-  assert_equal s_len 2;
-  let expected_e_info:ST.split_info option = Some {e; l_pre = 2} in
+  let s_len, e_info = ST.lcp_path st "hey t" es in
+  assert_equal s_len 5;
+  let expected_e_info:ST.split_info option = Some {e=e2; l_pre=3} in
   assert_equal e_info expected_e_info
 
 let suite =
