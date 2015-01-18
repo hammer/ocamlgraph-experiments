6ab5f227ad60aeec9519150448f26e1d68616151
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2015-01-17 19:29:40 -0500

    So many things wrong here but at least get_suffix works now.

--- src/lib/suffix_tree.ml
+++ src/lib/suffix_tree.ml
@@ -41,16 +41,32 @@ module ST = struct
       | () -> (lcp_len, Some {e; l_pre = lcp_len})
     in
     match es with
-    | [] -> (-1, None)
+    | [] -> (0, None)
     | _ -> BatList.reduce max (List.map (lcp_path_e st s) es)
+
+  let split_edge st e i =
+    let label = G.E.label e in
+    let v1 = G.E.src e in
+    let v2 = G.E.dst e in
+    let v_mid = G.V.create () in
+    let pre_l, post_l = String.split_at ~index:i label in
+    let e_pre = G.E.create v1 pre_l v_mid in
+    let e_post = G.E.create v_mid post_l v2 in
+    G.remove_edge_e st.g e;
+    List.iter (G.add_edge_e st.g) [e_pre; e_post];
+    v_mid
 
   let put_suffix st i s =
     let leaf = G.V.create () in
+    st.leaves.(i) <- leaf;
     let es = G.succ_e st.g st.root in
     let s_len, split_info = lcp_path st s es in
-    let e = G.E.create st.root s leaf in
-    G.add_edge_e st.g e;
-    st.leaves.(i) <- leaf
+    let v1 = match s_len, split_info with
+      | 0, _ -> st.root
+      | _, Some split_info -> split_edge st split_info.e split_info.l_pre
+    in
+    let e = G.E.create v1 s leaf in
+    G.add_edge_e st.g e
 
   let create s =
     let s_length = String.length s in
@@ -66,7 +82,15 @@ module ST = struct
     st
 
   let get_suffix st i =
-    G.fold_pred_e (fun e s -> (G.E.label e) ^ s) st.g st.leaves.(i) ""
+    let path_to_root =
+      let rec loop g v =
+        match G.pred_e g v with
+        | [] -> []
+        | hd :: tl -> (G.E.label hd) :: (loop g (G.E.src hd))
+      in
+      loop st.g st.leaves.(i)
+    in
+    String.concat (List.rev path_to_root)
 end
 
 
--- src/test/test_suffix_tree.ml
+++ src/test/test_suffix_tree.ml
@@ -1,22 +1,7 @@
 open OUnit2
 open Suffix_tree
 
-let test_lcp test_ctxt =
-  let s1 = "abc1" in
-  let s2 = "abc2" in
-  assert_equal 3 (ST.lcp s1 s2)
-
-let test_suffixes test_ctxt =
-  let s = "hey hey hey" in
-  let s_length = String.length s in
-  let st = ST.create s in
-  for i = 0 to s_length - 1 do
-    let st_suffix = ST.get_suffix st i in
-    let suffix = String.sub s i (s_length - i) in
-    assert_equal suffix st_suffix
-  done
-
-let test_lcp_path test_ctxt =
+let create_depth_two_st () =
   let g = ST.G.create () in
   let root = ST.G.V.create () in
   ST.G.add_vertex g root;
@@ -29,17 +14,42 @@ let test_lcp_path test_ctxt =
   let e2 = ST.G.E.create inner_node "y there" leaf in
   ST.G.add_edge_e st.g e2;
   st.leaves.(0) <- leaf;
+  st
+
+let test_lcp test_ctxt =
+  let s1 = "abc1" in
+  let s2 = "abc2" in
+  assert_equal 3 (ST.lcp s1 s2)
+
+let test_lcp_path test_ctxt =
+  let st:ST.t = create_depth_two_st () in
+  let e2 = List.hd (ST.G.pred_e st.g st.leaves.(0)) in
   let es = ST.G.succ_e st.g st.root in
   let s_len, e_info = ST.lcp_path st "hey t" es in
   assert_equal s_len 5;
   let expected_e_info:ST.split_info option = Some {e=e2; l_pre=3} in
   assert_equal e_info expected_e_info
 
+let test_get_suffix test_ctxt =
+  let st:ST.t = create_depth_two_st () in
+  assert_equal "hey there" (ST.get_suffix st 0)
+
+let test_suffixes test_ctxt =
+  let s = "xabxa$" in
+  let s_length = String.length s in
+  let st = ST.create s in
+  for i = 0 to s_length - 1 do
+    let st_suffix = ST.get_suffix st i in
+    let suffix = String.sub s i (s_length - i) in
+    assert_equal suffix st_suffix
+  done
+
 let suite =
   "suite" >:::
     [ "test_lcp" >:: test_lcp;
-      "test_suffixes" >:: test_suffixes;
-      "test_lcp_path" >:: test_lcp_path
+      "test_lcp_path" >:: test_lcp_path;
+      "test_get_suffix" >:: test_get_suffix;
+      "test_suffixes" >:: test_suffixes
     ]
 
 let () =
